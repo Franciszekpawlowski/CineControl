d836f5666c94d4dd72c0956fa59f6bca030c024d
UnReversed <171379891+UnReversed@users.noreply.github.com>
2024-06-04 19:41:40 +0000

    feat(CineControl.Domain): add basic config

---
+++ src/CineControl.Domain/Users/Users.cs
@@ -0,0 +1,22 @@
+﻿using CineControl.Domain.Common;
+
+namespace CineControl.Domain.Users;
+
+public class Users : Entity
+{   
+    public string FirstName { get; } = string.Empty;
+    public string LastName { get;  } = string.Empty;
+    public string Email { get; } = string.Empty;
+    public Users(
+        Guid id,
+        string firstName,
+        string lastName,
+        string email
+    ){
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+    }
+
+    private Users() { }
+}
