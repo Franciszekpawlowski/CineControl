a439ffe09bf656b28851fe9e9665168af0a9088b
Franciszekpawlowski <franciszekpawlowskiit@gmail.com>
2024-06-05 13:25:35 +0000

    add: FilmService.API

---
+++ src/CineControl.FilmService.API/Models/Film.cs
@@ -0,0 +1,12 @@
+namespace CineControl.FilmService.API.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Duration { get; set; } // in minutes
+        public string PosterUrl { get; set; }
+    }
+}
