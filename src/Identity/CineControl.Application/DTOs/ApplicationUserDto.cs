ead103788c297ee2c065a32de77092224a245e7a
UnReversed <171379891+UnReversed@users.noreply.github.com>
2024-06-05 07:41:02 +0000

    start again

--- src/Identity/CineControl.Application/DTOs/ApplicationUserDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CineControll.Application.DTOs;
-
-public class ApplicationUserDto
-{
-    public string Id { get; set; }
-    public string UserName { get; set; }
-    public string Email { get; set; }
-    public bool IsEmailConfirmed { get; set; }
-}
--- src/Identity/CineControl.Domain/Common/AuditEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CineControl.Domain;
-
-public abstract class AuditEntity
-{
-    public DateTime created { get; set; }
-    public string createdBy { get; set; }
-    public DateTime lastModified { get; set; }
-    public string lastModifiedBy { get; set; }
-}
