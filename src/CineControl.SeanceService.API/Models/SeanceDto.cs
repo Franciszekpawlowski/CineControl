3bc23c9fd3ab5845cd3e8a85d90f0ec928600004
Franciszekpawlowski <franciszekpawlowskiit@gmail.com>
2024-10-26 18:14:39 +0000

    feat: Update SeanceDto with theater info

--- src/CineControl.SeanceService.API/Models/SeanceDto.cs
+++ src/CineControl.SeanceService.API/Models/SeanceDto.cs
@@ -5,6 +5,8 @@ namespace CineControl.SeanceService.API.Models
         public int Id { get; set; }
         public int MovieId { get; set; }
         public string MovieTitle { get; set; }
+        public int TheaterId { get; set; } 
+        public string TheaterName { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
     }
